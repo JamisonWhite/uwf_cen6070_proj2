f944d4b417c0135cc5f82b774e8b2abb1c9a28c9
Jamie White <robertjamisonwhite@gmail.com>
2015-10-28 21:03:05 -0500

    added failing command line reader

--- MultiConcentration/src/multiconcentration/MultiConcentration.java
+++ MultiConcentration/src/multiconcentration/MultiConcentration.java
@@ -16,22 +16,62 @@ public class MultiConcentration {
      */
     public static void main(String[] args) {
 
-        printHelp();      
+        readParameters();
+        if (!validateParameters()) {
+            showHelp();
+            return;
+        }
         
+        if (showHelp) {
+            showHelp();
+        }
         
-        
+        if (useTextApplication) {
+            startTextApplication();
+        }
+
+        if (useGuiApplication) {
+            startGuiApplication();
+        }
+
+
     }
-    
-    public static void printHelp() {
-        
-        System.out.println("MultiConcentration Game");
+
+    public static boolean useTextApplication;
+
+    public static boolean useGuiApplication;
+
+    public static boolean showHelp;
+
+    public static void readParameters() {
+        System.out.println("NotImplementedException: read parameters is not implemented.");
+    }
+
+    public static boolean validateParameters() {
+        System.out.println("NotImplementedException: validate parameters is not implemented.");
+        return false;
+    }
+
+    public static void startGuiApplication() {
+        System.out.println("NotImplementedException: start GUI application is not implemented.");
+    }
+
+    public static void startTextApplication() {
+        System.out.println("NotImplementedException: start text application is not implemented.");
+    }
+
+    public static void showHelp() {
+
+        System.out.println("********************************");
+        System.out.println("* MultiConcentration Game");
+        System.out.println("********************************");
         System.out.println("Command Line:");
         System.out.println(" java -ea MultiConcentration [-g|-t] gridSize");
         System.out.println("Options");
         System.out.println(" -g for the GUI interface");
         System.out.println(" -t for the text interface");
         System.out.println(" size - number of rows and columns - must have a valid value");
-        
+
     }
-    
+
 }
